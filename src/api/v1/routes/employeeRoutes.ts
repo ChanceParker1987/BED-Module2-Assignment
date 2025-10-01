85260d8033614d24c51a0768650e411e200425aa
Chance Parker <cparker2@academic.rrc.ca>
2025-09-30 20:52:27 -0500

    Created routes in employeeRoutes.ts

---
+++ src/api/v1/routes/employeeRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+import * as employeeController from "../controllers/employeeController";
+
+const router: Router = express.Router();
+
+router.get("/employees", employeeController.getAllEmployees);
+router.get("/employees/:id", employeeController.getEmployeeById);
+router.post("/employees", employeeController.createEmployee);
+router.put("/employees/:id", employeeController.updateEmployee);
+router.delete("/employees/:id", employeeController.deleteEmployee);
+
+export default router;
